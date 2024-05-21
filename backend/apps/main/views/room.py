12bf3d32fc43643d92ce6820bbdd2987666603a0
Baxosh <bakhodir.2000.19@gmail.com>
2024-05-21 12:43:17 +0500

    Fixed params bug

--- backend/apps/main/views/room.py
+++ backend/apps/main/views/room.py
@@ -10,14 +10,13 @@ from main.swagger.room import RoomDetailSwagger, RoomSwagger
 from core.utils.pagination import pagination
 from main.models import Room
 from main.serializers.room import RoomFilterParams, RoomSerializer
-from main.utils.normalize_params import normalize_params
 
 
 class RoomListView(APIView):
     @swagger_auto_schema(responses=RoomSwagger, query_serializer=RoomFilterParams)
     @check_for_tenant
     def get(self, request):
-        params = RoomFilterParams.check(normalize_params(request.GET))
+        params = RoomFilterParams.check(request.GET)
         queryset = Room.objects.list(
             tenant=request.user.tenant,
             status=params.get("status"),
