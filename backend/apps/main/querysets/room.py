e6e0c586e244c68a1659f11bdfe960da893ab50f
Baxosh <bakhodir.2000.19@gmail.com>
2024-07-05 10:43:17 +0500

    Fixed sort_by room, GET request

--- backend/apps/main/querysets/room.py
+++ backend/apps/main/querysets/room.py
@@ -3,13 +3,20 @@ from django.db.models import Q
 
 
 class RoomQuerySet(BaseQuerySet):
-	def list(self, tenant, state, status=None, search_field=None, search_value=None, sort_by=None):
-		query = self.filter(active=True)
-		query = query.filter(state=state, tenant=tenant)
+    def list(self, tenant, state, status=None, search_field=None, search_value=None, sort_by=None):
+        query = self.filter(active=True)
+        query = query.filter(state=state, tenant=tenant)
 
-		if search_field and search_value:
-			query = query.filter(Q(**{f"{search_field}__startswith": search_value}))
+        if search_field and search_value:
+            query = query.filter(Q(**{f"{search_field}__startswith": search_value}))
 
-		query = query.order_by(*sort_by) if sort_by else query
-		query = query.filter(status=status) if status else query
-		return query.order_by("room_number")
+        if sort_by:
+            for item in sort_by:
+                try:
+                    query = query.extra(select={item: f"CAST(substring({item} FROM '^[0-9]+') AS INTEGER)"}).order_by(
+                        item
+                    )
+                except Exception as err:
+                    raise err
+        query = query.filter(status=status) if status else query
+        return query
--- backend/apps/main/views/room.py
+++ backend/apps/main/views/room.py
@@ -10,51 +10,51 @@ from rest_framework.views import APIView
 
 
 class RoomListView(APIView):
-	@swagger_auto_schema(responses=RoomSwagger, query_serializer=RoomFilterParams)
-	@check_for_tenant
-	def get(self, request):
-		params = RoomFilterParams.check(request.GET)
-		queryset = Room.objects.list(
-			tenant=request.user.tenant,
-			state=params.get("state"),
-			status=params.get("status"),
-			search_field=params.get("search_field"),
-			search_value=params.get("search_value"),
-			sort_by=params.get("sort_by"),
-		)
-		serializer = RoomSerializer(queryset, many=True)
-		data = pagination(queryset, serializer, params.get("page"), params.get("size", 15))
-		return Response(data)
+    @swagger_auto_schema(responses=RoomSwagger, query_serializer=RoomFilterParams)
+    @check_for_tenant
+    def get(self, request):
+        params = RoomFilterParams.check(request.GET)
+        queryset = Room.objects.list(
+            tenant=request.user.tenant,
+            state=params.get("state"),
+            status=params.get("status"),
+            search_field=params.get("search_field"),
+            search_value=params.get("search_value"),
+            sort_by=params.get("sort_by"),
+        )
+        serializer = RoomSerializer(queryset, many=True)
+        data = pagination(queryset, serializer, params.get("page"), params.get("size", 15))
+        return Response(data)
 
-	@swagger_auto_schema(responses=RoomSwagger, request_body=RoomSerializer)
-	@check_for_tenant
-	def post(self, request):
-		serializer = RoomSerializer(data=request.data)
-		serializer.is_valid(raise_exception=True)
-		serializer.save(tenant_id=request.user.tenant_id)
-		return Response(serializer.data, 201)
+    @swagger_auto_schema(responses=RoomSwagger, request_body=RoomSerializer)
+    @check_for_tenant
+    def post(self, request):
+        serializer = RoomSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(tenant_id=request.user.tenant_id)
+        return Response(serializer.data, 201)
 
 
 class RoomDetailView(APIView):
-	@swagger_auto_schema(responses=RoomDetailSwagger)
-	@check_for_tenant
-	def get(self, request, pk):
-		queryset = get_object_or_404(Room, id=pk, active=True)
-		serializer = RoomSerializer(queryset)
-		return Response(serializer.data)
+    @swagger_auto_schema(responses=RoomDetailSwagger)
+    @check_for_tenant
+    def get(self, request, pk):
+        queryset = get_object_or_404(Room, id=pk, active=True)
+        serializer = RoomSerializer(queryset)
+        return Response(serializer.data)
 
-	@swagger_auto_schema(responses=RoomDetailSwagger, request_body=RoomSerializer)
-	@check_for_tenant
-	def put(self, request, pk):
-		instance = get_object_or_404(Room, id=pk, active=True)
-		serializer = RoomSerializer(instance, data=request.data)
-		serializer.is_valid(raise_exception=True)
-		serializer.save(updated_by=request.user)
-		return Response(serializer.data)
+    @swagger_auto_schema(responses=RoomDetailSwagger, request_body=RoomSerializer)
+    @check_for_tenant
+    def put(self, request, pk):
+        instance = get_object_or_404(Room, id=pk, active=True)
+        serializer = RoomSerializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(updated_by=request.user)
+        return Response(serializer.data)
 
-	@swagger_auto_schema(responses={})
-	@check_for_tenant
-	def delete(self, request, pk):
-		instance = get_object_or_404(Room, id=pk)
-		instance.delete()
-		return Response({}, 204)
+    @swagger_auto_schema(responses={})
+    @check_for_tenant
+    def delete(self, request, pk):
+        instance = get_object_or_404(Room, id=pk)
+        instance.delete()
+        return Response({}, 204)
