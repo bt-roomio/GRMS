7d9cece06c9f036073b2295a1e1b99dd12c84732
Baxosh <bakhodir.2000.19@gmail.com>
2024-07-08 18:26:11 +0500

    Removed auth from device_credentials GET

--- backend/apps/main/views/device_credentials.py
+++ backend/apps/main/views/device_credentials.py
@@ -1,10 +1,13 @@
 from main.models import DeviceCredentials
 from main.serializers.device_credentials import DeviceCredentialsSerializer
 from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView, Response
 
 
 class DeviceCredentialsDetailView(APIView):
+    permission_classes = [AllowAny]
+
     def get(self, request, token):
         instance = get_object_or_404(DeviceCredentials, credentials_id=token)
         serializer = DeviceCredentialsSerializer(instance)
