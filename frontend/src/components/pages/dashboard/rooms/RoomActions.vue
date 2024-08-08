68c96a0206b68e4ffbf7c00487a29080f9050251
Ruslan <ruslan.web.time@gmail.com>
2024-08-08 16:55:16 +0500

    fix

--- frontend/src/components/pages/dashboard/rooms/RoomActions.vue
+++ frontend/src/components/pages/dashboard/rooms/RoomActions.vue
@@ -31,7 +31,6 @@ import UiIcon from "@components/ui/Icon.vue";
 import {computed, onMounted, onUnmounted} from "vue";
 import {useWS} from "@store/dashboard/ws";
 import {storeToRefs} from "pinia";
-import UiSmallButton from "@components/ui/ButtonSmall.vue";
 const props = defineProps(['item'])
 const entityId = computed(() => (props.item as IRoom)?.devices?.[0]?.id || null)
 const storeWs = useWS()
