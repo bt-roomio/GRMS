0926bb732d7dced4a23ac406f2e26e0846fbdf05
bakha_aka <bt@room.io>
2024-09-12 12:16:04 +0200

    filter by value roomio_node

--- apps/main/utils/has_roomio_node.py
+++ apps/main/utils/has_roomio_node.py
@@ -4,6 +4,6 @@ from main.models import Device
 def has_roomio_node(tenant_id):
     devices = Device.objects.filter(tenant_id=tenant_id).values_list("additional_info", flat=True)
     for d in list(devices):
-        if isinstance(d, dict) and "roomio_node" in d:
+        if isinstance(d, dict) and "roomio_node" in d and d.get("roomio_node"):
             return True
     return False
