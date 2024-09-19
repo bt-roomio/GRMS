fe989fce32b81f092eae7b097f0b237b5f323aac
bakha_aka <bt@room.io>
2024-09-19 09:53:29 +0200

    sending ATTRIBUTE without filter CLIENT_SCOPE

--- apps/shuttle/consumers/aggregations/gateway_list.py
+++ apps/shuttle/consumers/aggregations/gateway_list.py
@@ -9,7 +9,7 @@ from shuttle.utils.get_non_null_field import get_non_null_field
 @database_sync_to_async
 def gateway_list(entity_fields, attributes):
     data = []
-    attr = AttributeKv.objects.filter(attribute_type=AttributeKv.CLIENT_SCOPE, attribute_key__in=attributes)
+    attr = AttributeKv.objects.filter(attribute_key__in=attributes)
     devices = Device.objects.prefetch_related(Prefetch(queryset=attr, lookup="attribute_kvs")).filter(
         additional_info__gateway=True
     )
