0716ce32dee5ffa3dd30433dd92ee26381938f2d
bakha_aka <bt@room.io>
2024-09-23 09:51:33 +0200

    Sending fake data till mqtt-rpc request in mqtt-broker

--- apps/shuttle/views/json_rpc.py
+++ apps/shuttle/views/json_rpc.py
@@ -6,7 +6,6 @@ from rest_framework.views import APIView
 
 from main.models import Device
 from shuttle.models import Relation
-from shuttle.utils.send_to_rabbitmq import send_to_rabbitmq_rpc
 
 
 class JsonRpcView(APIView):
@@ -31,11 +30,11 @@ class JsonRpcView(APIView):
 def get_value(gateway_device, device, method, params, request, timeout):
     print("Getting value")
     message_id = uuid.uuid4()
-    send_to_rabbitmq_rpc(message_id, gateway_device, device, method, params)
+    # send_to_rabbitmq_rpc(message_id, gateway_device, device, method, params)
     # Wait until response or changes in db, After sent rpc_message clear db.
 
     # 1. wait response in this function
     # 2. while every .5s check for db
     # instance = RPCMessage.objects.filter(id=message_id)
     # serializer = RPCMessageSerializer(instance)
-    return Response({})
+    return Response({"device": "Device A", "id": message_id, "data": {"success": True}})
