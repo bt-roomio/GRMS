2ed5f434021ee61fe770f9a8962cbd810eb08482
bakha_aka <bt@room.io>
2024-10-08 14:16:14 +0200

    Fixed active status for device and AttributeKv

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt
 
 
-RUN apt-get update && apt-get install -y supervisor
+RUN apt-get update && apt-get install -y supervisor htop
 
 RUN mkdir -p /var/log/django
 RUN chmod -R 755 /var/log/django
--- apps/core/management/commands/check_attribute_server_scope.py
+++ apps/core/management/commands/check_attribute_server_scope.py
@@ -1,7 +1,6 @@
 import time
 
 from django.core.management.base import BaseCommand
-from django.db.models import Count
 
 from shuttle.models import AttributeKv
 
@@ -11,21 +10,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         while True:
-            print("Checking Attributes SERVER_SCOPE", time.time())
+            print("Checking Attributes SERVER_SCOPE", int(time.time()))
             check_attribute_server_scope()
-            time.sleep(30)
+            time.sleep(10)
 
 
 def check_attribute_server_scope():
-    attribute_kv = (
-        AttributeKv.objects.values("entity", "attribute_type", "attribute_key", "long_v", "bool_v")
-        .filter(attribute_key="lastActivityTime", attribute_type=AttributeKv.SERVER_SCOPE)
-        .annotate(count=Count("entity"))
+    attribute_kv = AttributeKv.objects.filter(
+        attribute_key="active", attribute_type=AttributeKv.SERVER_SCOPE, bool_v=True
     )
-    for attr in attribute_kv:
-        if attr.get("attribute_key") == "lastActivityTime" and (attr.get("long_v") or 0 <= int(time.time()) - 30):
-            AttributeKv.objects.filter(
-                entity=attr["entity"],
-                attribute_key="active",
-                attribute_type=AttributeKv.SERVER_SCOPE,
-            ).update(bool_v=False)
+
+    for attr_active in attribute_kv:
+        attr = AttributeKv.objects.get(entity_id=attr_active.entity, attribute_key="lastActivityTime")
+        if attr.long_v <= int(time.time()) - 30:
+            attr_active.bool_v = False
+            attr_active.save()
