14b57d7f3cbcf494a39048eb9d5acf6099c02609
bakha_aka <bt@room.io>
2024-10-19 22:58:59 +0200

    Fixed room status

--- apps/main/querysets/room.py
+++ apps/main/querysets/room.py
@@ -1,6 +1,6 @@
 import datetime
 
-from django.db.models import Q, Count, Case, When, Value, F
+from django.db.models import Q, Count, Func
 
 from core.querysets.base_queryset import BaseQuerySet
 
@@ -31,19 +31,47 @@ class RoomQuerySet(BaseQuerySet):
         return query
 
     def room_status(self, tenant):
+        from main.models import Room
+
         query = self.filter(active=True, tenant=tenant)
         today_midnight = datetime.datetime.combine(datetime.date.today(), datetime.time.min).timestamp()
         yesterday_midnight = today_midnight - 86400
 
-        query = (
-            query.annotate(
-                today=Count(Case(When(updated_at__gte=today_midnight, then=Value(1)))),
-                yesterday=Count(
-                    Case(When(updated_at__gte=yesterday_midnight, updated_at__lt=today_midnight, then=Value(1)))
-                ),
-                last_24_hour=Count("state"),
-            )
-            .values("state", "last_24_hour")
-            .annotate(diff_previous_day=F("today") - F("yesterday"), status=F("state"))
+        rooms_updated_today = (
+            query.filter(updated_at__gte=today_midnight)
+            .annotate(state_element=Func("state", function="unnest"))
+            .values("state_element")
+            .annotate(count_today=Count("id"))
         )
-        return query
+
+        rooms_updated_yesterday = (
+            query.filter(updated_at__gte=yesterday_midnight, updated_at__lt=today_midnight)
+            .annotate(state_element=Func("state", function="unnest"))
+            .values("state_element")
+            .annotate(count_yesterday=Count("id"))
+        )
+
+        today_counts = {room["state_element"]: room["count_today"] for room in rooms_updated_today}
+        yesterday_counts = {room["state_element"]: room["count_yesterday"] for room in rooms_updated_yesterday}
+
+        all_states_tenant = (
+            query.annotate(state_element=Func("state", function="unnest"))
+            .values("state_element")
+            .annotate(count=Count("id"))
+            .order_by("state_element")
+        )
+        result = []
+        for s in all_states_tenant:
+            data = {"status": Room.STATE[s["state_element"]][1], "last_24_hour": s["count"]}
+            result.append(data)
+
+        all_state_elements = set(today_counts.keys()).union(yesterday_counts.keys())
+        for state_element in all_state_elements:
+            count_today = today_counts.get(state_element, 0)
+            count_yesterday = yesterday_counts.get(state_element, 0)
+            difference = count_today - count_yesterday
+            for elem in result:
+                if elem["status"] == Room.STATE[state_element][1]:
+                    elem["diff_previous_day"] = difference
+
+        return result
