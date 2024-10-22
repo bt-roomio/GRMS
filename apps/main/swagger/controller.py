57a230f8606883ca795d2834b7369bf48bf8e9a4
bakha_aka <bt@room.io>
2024-10-22 14:07:07 +0200

    Added description for API controller/device_id/name/

--- apps/main/swagger/controller.py
+++ apps/main/swagger/controller.py
@@ -1,10 +1,21 @@
+from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
 
 def swagger():
     return swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                "device_id",
+                openapi.IN_PATH,
+                description="UUID of the device",
+                type=openapi.TYPE_STRING,
+                format=openapi.FORMAT_UUID,
+            ),
+            openapi.Parameter("name", openapi.IN_PATH, description="Name of the attribute", type=openapi.TYPE_STRING),
+        ],
         responses={
             200: "List of controllers.",
             400: '{"detail": "Error message!"}',
-        }
+        },
     )
