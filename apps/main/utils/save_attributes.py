0c7f37312951aa51e886e9d9c0299e07211c6145
bakha_aka <bt@room.io>
2024-11-04 11:09:48 +0200

    Fixed: update_or_create attributes

--- apps/main/utils/save_attributes.py
+++ apps/main/utils/save_attributes.py
@@ -5,9 +5,9 @@ from shuttle.models import AttributeKv
 
 def save_attributes(devices, available_fields, scope):
     attributes = {}
-    fields = {"bool_v": None, "str_v": None, "long_v": None, "dbl_v": None, "json_v": None}
 
     for key, item in available_fields.items():
+        fields = {"bool_v": None, "str_v": None, "long_v": None, "dbl_v": None, "json_v": None}
         field, value = item[0], item[1]
         fields[field] = value
 
