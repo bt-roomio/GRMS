82577ba1430c74056a8be950caff02b25c028a6a
bakha_aka <bt@room.io>
2024-11-05 09:20:06 +0200

    Added response in tag_swagger

--- apps/shuttle/swagger/tag.py
+++ apps/shuttle/swagger/tag.py
@@ -1,11 +1,33 @@
+from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
 from shuttle.serializers.tag import TagFilterParams
+
+response_200 = openapi.Schema(
+    type=openapi.TYPE_OBJECT,
+    properties={
+        "data": openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "attribute_name": openapi.Schema(
+                    type=openapi.TYPE_ARRAY,
+                    items=openapi.Schema(
+                        type=openapi.TYPE_ARRAY,
+                        items=[openapi.Schema(type=openapi.TYPE_INTEGER), openapi.Schema(type=openapi.TYPE_BOOLEAN)],
+                    ),
+                )
+            },
+        ),
+        "latestValues": openapi.Schema(
+            type=openapi.TYPE_OBJECT, properties={"attribute_name": openapi.Schema(type=openapi.TYPE_INTEGER)}
+        ),
+    },
+)
 
 
 def tag_swagger():
     return swagger_auto_schema(
         query_serializer=TagFilterParams(),
-        responses={},
+        responses={200: response_200},
         operation_description="""""",
     )
