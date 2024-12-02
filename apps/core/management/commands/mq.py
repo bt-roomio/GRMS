19c7ebfcc29c7b1f02a0e4b08482e7e96d2f7154
bakha_aka <bt@room.io>
2024-12-02 16:50:07 +0200

    RabbitMQ messages process via celery

--- apps/core/management/commands/mq.py
+++ apps/core/management/commands/mq.py
@@ -1,7 +1,13 @@
+import logging
+
+import pika
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from main.utils.listening_mq import MQListener
+from shuttle.tasks import process_mq
+
+logger = logging.getLogger(__name__)
+logger.critical("Now logging consumer command")
 
 RABBIT_LOGIN = settings.RABBIT_LOGIN
 RABBIT_PASSWORD = settings.RABBIT_PASSWORD
@@ -15,5 +21,23 @@ class Command(BaseCommand):
     help = "Closes the specified poll for voting"
 
     def handle(self, *args, **options):
-        td = MQListener()
-        td.start()
+        consume()
+
+
+def consume():
+    channel = connect_to_rabbitmq()
+
+    def message_handler(ch, method, properties, body):
+        process_mq.delay(body)
+
+    channel.basic_consume(queue=rabbit_queues["toGRMSqueueName"], on_message_callback=message_handler, auto_ack=True)
+    logger.critical("Waiting for messages in topic. To exit press CTRL+C")
+    channel.start_consuming()
+
+
+def connect_to_rabbitmq():
+    credentials = pika.PlainCredentials(RABBIT_LOGIN, RABBIT_PASSWORD)
+    parameters = pika.ConnectionParameters(RABBIT_HOST, RABBIT_PORT, "/", credentials)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    return channel
--- config/celery.py
+++ config/celery.py
@@ -1,14 +1,20 @@
 import os
 
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-app = Celery("config")
+RABBIT_LOGIN = settings.RABBIT_LOGIN
+RABBIT_PASSWORD = settings.RABBIT_PASSWORD
+RABBIT_HOST = settings.RABBIT_HOST
+RABBIT_PORT = settings.RABBIT_PORT
+
+CELERY_BROKER_URL = f"amqp://{RABBIT_LOGIN}:{RABBIT_PASSWORD}@{RABBIT_HOST}:{RABBIT_PORT}//"
+
+app = Celery("config", broker=CELERY_BROKER_URL)
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
-
-
 app.autodiscover_tasks()
 
 
