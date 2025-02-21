c33cdb3dbfae3db0fd78d0ea0f8425da9cb9ae4c
bakha_aka <bt@room.io>
2025-02-21 10:08:27 +0200

    removed throttling

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -175,8 +175,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 15,
     "DATETIME_FORMAT": "%s",
-    "DEFAULT_THROTTLE_CLASSES": ["rest_framework.throttling.AnonRateThrottle"],
-    "DEFAULT_THROTTLE_RATES": {"anon": "20/day"},
 }
 
 SIMPLE_JWT = {
