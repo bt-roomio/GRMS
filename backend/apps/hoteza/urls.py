eb69660897f76000855eccba03f156b1c36ece4e
bakha_aka <bt@room.io>
2025-03-07 10:41:23 +0200

    added hotelId in general_settings, fixed urls in hoteza API

--- backend/apps/hoteza/urls.py
+++ backend/apps/hoteza/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-
 from hoteza.views.checkin import CheckInListView
 from hoteza.views.checkout import CheckOutListView
 from hoteza.views.dnd import DNDListView
 from hoteza.views.guest_change import GuestChangeListView
 
 urlpatterns = [
-    path("checkin/", CheckInListView.as_view(), name="checkin-list-view"),
-    path("checkout/", CheckOutListView.as_view(), name="checkout-list-view"),
-    path("guestchange/", GuestChangeListView.as_view(), name="guest-change-view"),
-    path("dnd/", DNDListView.as_view(), name="dnd-view"),
+    path("checkin", CheckInListView.as_view(), name="checkin-list-view"),
+    path("checkout", CheckOutListView.as_view(), name="checkout-list-view"),
+    path("guestchange", GuestChangeListView.as_view(), name="guest-change-view"),
+    path("dnd", DNDListView.as_view(), name="dnd-view"),
 ]
--- backend/apps/hoteza/views/checkin.py
+++ backend/apps/hoteza/views/checkin.py
@@ -1,14 +1,19 @@
+import logging
+
+from hoteza.serializers.checkin import CheckInSerializer
+
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from hoteza.serializers.checkin import CheckInSerializer
+logger = logging.getLogger(__name__)
 
 
 class CheckInListView(APIView):
     permission_classes = (AllowAny,)
 
     def post(self, request):
+        logger.debug(request.data)
         serializer = CheckInSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
