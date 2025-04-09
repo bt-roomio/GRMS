2e205c51d1f954202eb85611235b027371252e46
bakha_aka <bt@room.io>
2025-04-09 13:34:59 +0200

    fixed AttributeKv WS V2

--- backend/apps/shuttle/signals.py
+++ backend/apps/shuttle/signals.py
@@ -3,7 +3,7 @@ from channels.layers import get_channel_layer
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from shuttle.models import TsKv, TsKvLatest
+from shuttle.models import AttributeKv, TsKv, TsKvLatest
 
 
 @receiver(post_save, sender=TsKv)
@@ -39,3 +39,21 @@ def tskv_latest_signal_handler(sender, instance, **kwargs):
             "json_v": instance.json_v,
         }
         async_to_sync(channel_layer.group_send)("tskv_latest_updates", {"type": "get_latest_activity", **message})
+
+
+@receiver(post_save, sender=AttributeKv)
+def attribute_kv_signal_handler(sender, instance, **kwargs):
+    channel_layer = get_channel_layer()
+    if channel_layer is not None:
+        message = {
+            "entity": str(instance.entity_id),
+            "last_update_ts": instance.last_update_ts,
+            "scope": instance.attribute_type,
+            "key_name": instance.attribute_key,
+            "bool_v": instance.bool_v,
+            "str_v": instance.str_v,
+            "long_v": instance.long_v,
+            "dbl_v": instance.dbl_v,
+            "json_v": instance.json_v,
+        }
+        async_to_sync(channel_layer.group_send)("attribute_kv_updates", {"type": "get_latest_activity", **message})
--- backend/apps/shuttle/v2_consumers/attributes.py
+++ backend/apps/shuttle/v2_consumers/attributes.py
@@ -1,9 +1,8 @@
 from asgiref.sync import sync_to_async
 from djangochannelsrestframework.mixins import ListModelMixin, action
-from djangochannelsrestframework.observer import model_observer
 
 from shuttle.models import AttributeKv
-from shuttle.serializers.attributes import AttributeFilterParams, AttributeSerializer, SimpleAttributeSerializer
+from shuttle.serializers.attributes import AttributeFilterParams, AttributeSerializer
 from shuttle.utils.get_non_null_field import get_non_null_column
 from shuttle.v2_consumers.base_generics import BaseGenericAsyncAPIConsumer
 
@@ -27,45 +26,43 @@ class AttributeConsumer(ListModelMixin, BaseGenericAsyncAPIConsumer):
         query = self.pagination(query, params.get("page", 1), params.get("size", 25))
         return query
 
-    @model_observer(AttributeKv, serializer_class=SimpleAttributeSerializer)
-    async def get_latest_activity(self, message, action, **kwargs):
+    async def get_latest_activity(self, message, **kwargs):
         for request_id, params in self.request_ids.items():
-            device = params.get("device")
-            scope = params.get("scope")
-            if device == message.get("entity") and scope == message.get("attribute_type"):
+            device = params.get("query_params").get("device")
+            scope = params.get("query_params").get("scope")
+            action = params.get("action")
+
+            if device == message.get("entity") and action == "list_subscribe" and scope == message.get("scope"):
+                data = await sync_to_async(self.get_data)(query_params=params.get("query_params"), **kwargs)
+                if any([message.get("key_name") == i["key_name"] for i in data]):
+                    await self.reply(data=data, action="list_subscribe", request_id=request_id)
+
+            elif device == message.get("entity") and action == "subscribe" and scope == message.get("scope"):
                 _, value = get_non_null_column(message)
                 message = {
-                    "attribute_key": message.get("attribute_key"),
+                    "key_name": message.get("attribute_key"),
                     "last_update_ts": message.get("last_update_ts"),
                     "value": value,
                 }
-                await self.reply(data=message, action=action, request_id=request_id)
+                await self.reply(data=message, action="subscribe", request_id=request_id)
 
     @action()
-    async def subscribe(self, request_id, query_params, **kwargs):
-        await self.get_latest_activity.subscribe(request_id=request_id, **kwargs)
-        self.request_ids[request_id] = query_params
+    async def subscribe(self, request_id, action, query_params, **kwargs):
+        if self.channel_layer is not None:
+            await self.channel_layer.group_add("attribute_kv_updates", self.channel_name)
+            self.request_ids[request_id] = {"query_params": query_params, "action": action}
 
     @action()
     async def unsubscribe(self, request_id, **kwargs):
-        await self.get_latest_activity.unsubscribe(request_id=request_id, **kwargs)
         self.request_ids.pop(request_id, None)
-
-    @model_observer(AttributeKv, serializer_class=SimpleAttributeSerializer)
-    async def get_list_activity(self, message, action, **kwargs):
-        for request_id, params in self.request_ids.items():
-            device = params.get("device")
-            scope = params.get("scope")
-            if device == message.get("entity") and scope == message.get("attribute_type"):
-                data = await sync_to_async(self.get_data)(query_params=params, **kwargs)
-                await self.reply(data=data, action=action, request_id=request_id)
 
     @action()
     async def list_subscribe(self, request_id, action, query_params, **kwargs):
         await self.send_list(action, query_params, request_id, **kwargs)
-        await self.get_list_activity.subscribe(request_id=request_id, **kwargs)
-        self.request_ids[request_id] = query_params
+        if self.channel_layer is not None:
+            await self.channel_layer.group_add("attribute_kv_updates", self.channel_name)
+            self.request_ids[request_id] = {"query_params": query_params, "action": action}
 
     @action()
     async def list_unsubscribe(self, request_id, **kwargs):
-        await self.get_list_activity.unsubscribe(request_id=request_id, **kwargs)
+        self.request_ids.pop(request_id, None)
