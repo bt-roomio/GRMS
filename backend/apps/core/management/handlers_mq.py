e9b8bc4fcbb6c001ff64f192522be4619232c6c8
bakha_aka <bt@room.io>
2025-04-28 17:53:34 +0200

    fix UUID bug

--- backend/apps/core/management/handlers_mq.py
+++ backend/apps/core/management/handlers_mq.py
@@ -142,15 +142,13 @@ def _handle_attribute_saving(device, data):
                 "last_update_ts": ts_now,
             }
             defaults[field] = value
-            print(defaults)
-            r = AttributeKv.objects.update_or_create(
+            AttributeKv.objects.update_or_create(
                 entity=device,
                 entity__tenant_id=device.tenant_id,
                 attribute_type=AttributeKv.CLIENT_SCOPE,
                 attribute_key=key,
                 defaults=defaults,
             )
-            print(r)
     _update_activity_gateway(device)
 
 
