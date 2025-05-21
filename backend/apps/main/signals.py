906ebab7b26061bf5660d64997d3c95317270b77
YGMuzenitov@rusatom-utilities.ru <Genius4321>
2025-05-21 16:08:03 +0300

    New message handle

--- backend/apps/main/signals.py
+++ backend/apps/main/signals.py
@@ -1,37 +1,20 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from channels.layers import get_channel_layer
+
 from core.rabbitmq.config import connect_to_rabbitmq, send_to_rabbitmq
-from main.models import Device, Room
+from main.models import Room
 from main.utils.default_state import StateEnum, attribute_room_state
-from main.utils.remove_or_add_state import remove_or_add
 from shuttle.models import AttributeKv
-from asgiref.sync import async_to_sync
-
-
-@receiver(post_save, sender=Device)
-def update_status_of_room(sender, instance, **kwargs):
-    if instance.room:
-        working_devices = all(Device.objects.filter(room_id=instance.room_id).values_list("status", flat=True))
-
-        instance.room.status = "ON" if working_devices else "OFF"
-        instance.room.save()
 
 
 @receiver(post_save, sender=AttributeKv)
 def update_state_of_room_and_status_device(sender, instance, **kwargs):
-    devices = Device.objects.filter(id=instance.entity_id)
-    if devices:
-        for device in devices:
-            if instance.attribute_key == "mur" and device.room:
-                remove_or_add(instance.bool_v, device.room, Room.MakeUpRoom)
-            if instance.attribute_key == "active" and instance.attribute_type == AttributeKv.SERVER_SCOPE:
-                if device.room:
-                    device.room.status = Room.ON if instance.bool_v else Room.OFF
-                    device.room.save()
-                device.status = bool(instance.bool_v)
-                device.save()
+    if instance.attribute_key == "active" and instance.attribute_type == AttributeKv.SERVER_SCOPE:
+        instance.entity.status = bool(instance.bool_v)
+        instance.entity.save()
     if instance.attribute_type == AttributeKv.CLIENT_SCOPE and instance.attribute_key == "scanned_devices":
         AttributeKv.objects.update_or_create(
             attribute_key=instance.attribute_key,
