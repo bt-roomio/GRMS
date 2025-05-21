d232a9dd1205a8197843fe64ea3b8a551ab433b5
YGMuzenitov@rusatom-utilities.ru <Genius4321>
2025-05-21 16:08:03 +0300

    New message handle

--- backend/apps/core/management/commands/mq.py
+++ backend/apps/core/management/commands/mq.py
@@ -62,9 +62,9 @@ class Command(BaseCommand):
                 logger.exception(f"[{queue_name}] Error in consumer thread: {err}")
 
     def process_message(self, queue_name, ch, method, properties, body):
-        while True:
-            try:
-                handlers_mq(ch, body)
+        try:
+            handlers_mq(ch, body)
+            if ch.is_open:
                 ch.basic_ack(delivery_tag=method.delivery_tag)
-            except Exception as err:
-                logger.warning(f"[{queue_name}] Error processing message: {body}. Error: {err}")
+        except Exception as err:
+            logger.warning(f"[{queue_name}] Error processing message: {body}. Error: {err}")
