b86f6a2fba868f6eb91188fd1f95bf1fc142242f
irgashev-dev <mrirgashevn@gmail.com>
2025-06-17 16:15:48 +0500

    fixed need sync filteration

--- backend/apps/access_manager/querysets/card.py
+++ backend/apps/access_manager/querysets/card.py
@@ -8,17 +8,18 @@ class CardQuerySet(BaseQuerySet):
         from access_manager.models import NeedSyncDevice
 
         query = self.select_related("created_by").filter(tenant_id=tenant_id)
-        if 'need_sync' in filters:
-            need_sync = filters.get("need_sync")
-            query = query.annotate(
-                need_sync=Exists(
-                    NeedSyncDevice.objects.filter(
-                        card=OuterRef('pk'),
-                        need_sync=True
-                    )
+        need_sync = filters.get("need_sync")
+
+        query = query.annotate(
+            need_sync=Exists(
+                NeedSyncDevice.objects.filter(
+                    card=OuterRef('pk'),
+                    need_sync=True
                 )
             )
-            query = query.filter(need_sync=need_sync)
+        )
+
+        query = query.filter(need_sync=need_sync) if need_sync is not None else query
 
         if search_field and search_value:
             query = query.filter(Q(**{f"{search_field}__istartswith": search_value}))
