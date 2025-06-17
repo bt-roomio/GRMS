5f24936fc1ef7d1a65495ea5ad431d9230f26e84
irgashev-dev <mrirgashevn@gmail.com>
2025-06-17 15:30:55 +0500

    feat: added filter by need sync status

--- backend/apps/access_manager/querysets/guest_card.py
+++ backend/apps/access_manager/querysets/guest_card.py
@@ -4,8 +4,9 @@ from core.querysets.base_queryset import BaseQuerySet
 
 
 class GuestCardQuerySet(BaseQuerySet):
-    def list(self, tenant_id, room=None, sort_by=[]):
+    def list(self, tenant_id, room=None, sort_by=[], filters={}):
         from access_manager.models import NeedSyncDevice
+        need_sync = filters.get("need_sync")
 
         query = self.filter(guest__tenant_id=tenant_id, is_active=True)
         query = query.filter(guest__room=room) if room else query
@@ -17,5 +18,6 @@ class GuestCardQuerySet(BaseQuerySet):
                 )
             )
         )
+        query = query.filter(need_sync=need_sync) if need_sync is not None else query
 
         return query.order_by(*sort_by)
