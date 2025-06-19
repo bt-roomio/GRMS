0495dfd84b267ee921f032704af79f659d2abfdd
irgashev-dev <mrirgashevn@gmail.com>
2025-06-19 16:18:05 +0500

    group is fixed

--- backend/apps/access_manager/serializers/group.py
+++ backend/apps/access_manager/serializers/group.py
@@ -96,9 +96,8 @@ class GroupSerializer(serializers.ModelSerializer):
         instance.group_room.filter(room_id__in=rooms_to_remove).delete()
 
         for room in rooms:
-            _, created = GroupRoom.objects.get_or_create(group=instance, room=room)
-            if created:
-                card_room(instance.id, room.id, action='connect')
+            GroupRoom.objects.get_or_create(group=instance, room=room)
+            card_room(instance.id, room.id, action='connect')
 
         for public_space_id in public_spaces_to_remove:
             card_public_space(instance.id, public_space_id, action="disconnect")
@@ -106,9 +105,8 @@ class GroupSerializer(serializers.ModelSerializer):
         instance.group_public_space.filter(public_space_id__in=public_spaces_to_remove).delete()
 
         for public_space in public_spaces:
-            _, created = GroupPublicSpace.objects.get_or_create(group=instance, public_space=public_space)
-            if created:
-                card_public_space(instance.id, public_space.id, action="connect")
+            GroupPublicSpace.objects.get_or_create(group=instance, public_space=public_space)
+            card_public_space(instance.id, public_space.id, action="connect")
 
         return instance
 
