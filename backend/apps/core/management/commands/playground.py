e80d42a728cc8539cac32803822b0985b10d2f65
irgashev-dev <mrirgashevn@gmail.com>
2025-07-25 11:28:34 +0500

    feat: device change of public spaces
    -right now when devices are changed cards are connected or disconnected accordingly
    -if device is deactivated its cards are added to need sync

--- backend/apps/core/management/commands/playground.py
+++ backend/apps/core/management/commands/playground.py
@@ -1,15 +1,20 @@
 from django.contrib.auth.models import Permission
 from django.core.management.base import BaseCommand
 
-from users.models import Role
+from shuttle.models import RPCMessage
 
 
 class Command(BaseCommand):
     help = "Playground"
 
     def handle(self, *args, **options):
-        tenant_admins = Role.objects.filter(name__in=["TENANT_ADMIN", "SYS_ADMIN"])
-        permissions = Permission.objects.all()
-        for tenant_admin in tenant_admins:
-            tenant_admin.permissions.clear()
-            tenant_admin.permissions.add(*permissions)
+
+        rpc_message = RPCMessage.objects.order_by('-created_at')[:5]
+        if not rpc_message:
+            self.stdout.write(self.style.ERROR("No RPCMessage found"))
+            return
+        for rpc in rpc_message:
+            print(rpc.id)
+            rpc.additional_info = {"success": True}
+            rpc.received = True
+            rpc.save()
