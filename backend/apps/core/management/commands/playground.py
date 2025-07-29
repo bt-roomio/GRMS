0cc1ff03286b12610d6f246437fb994151d4ab28
irgashev-dev <mrirgashevn@gmail.com>
2025-07-29 08:15:37 +0500

    clear playground

--- backend/apps/core/management/commands/playground.py
+++ backend/apps/core/management/commands/playground.py
@@ -1,20 +1,8 @@
-from django.contrib.auth.models import Permission
 from django.core.management.base import BaseCommand
-
-from shuttle.models import RPCMessage
 
 
 class Command(BaseCommand):
     help = "Playground"
 
     def handle(self, *args, **options):
-
-        rpc_message = RPCMessage.objects.order_by('-created_at')[:5]
-        if not rpc_message:
-            self.stdout.write(self.style.ERROR("No RPCMessage found"))
-            return
-        for rpc in rpc_message:
-            print(rpc.id)
-            rpc.additional_info = {"success": True}
-            rpc.received = True
-            rpc.save()
+        pass
