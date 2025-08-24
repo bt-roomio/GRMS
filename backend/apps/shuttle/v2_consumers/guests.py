0c3b495c52490225a763e9c89b5109a0e282c328
bakha_aka <bt@room.io>
2025-08-24 16:34:43 +0200

    fix: guest updates on checkout in WS

--- backend/apps/shuttle/v2_consumers/guests.py
+++ backend/apps/shuttle/v2_consumers/guests.py
@@ -52,8 +52,7 @@ class GuestConsumer(ListModelMixin, ObserverModelInstanceMixin, BaseGenericAsync
             room = params.get("room")
             if room == message.get("room"):
                 data = await sync_to_async(self.get_data_paginated)(query_params=params, **kwargs)
-                if any([message.get("id") == i["id"] for i in data.get("results", [])]):
-                    await self.reply(data=data, action=action, request_id=request_id)
+                await self.reply(data=data, action=action, request_id=request_id)
 
     @action()
     async def list_subscribe(self, request_id, action, query_params, **kwargs):
