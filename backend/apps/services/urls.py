7493892a49925c5553f123d984896f3b4d0e5d45
bakha_aka <bt@room.io>
2025-08-26 22:42:26 +0200

    fix: sber_window

--- backend/apps/services/urls.py
+++ backend/apps/services/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from services.views.integration import IntegrationDetailView, IntegrationListView
 from services.views.sber_get_room import SberGetRoomDetailView
+from services.views.sber_window import SberWindowView
 
 urlpatterns = [
     path("integrations/", IntegrationListView.as_view(), name="integration-list"),
     path("integration/<uuid:pk>/", IntegrationDetailView.as_view(), name="integration-detail"),
     path("sber/", SberGetRoomDetailView.as_view(), name="sber-get-room-detail"),
+    path("sber-window/", SberWindowView.as_view(), name="sber-window-detail"),
 ]
