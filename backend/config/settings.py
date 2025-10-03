2e580a0f824d32bd8a146b3b9f18089c7070303b
bakha_aka <bt@room.io>
2025-10-03 15:24:36 +0200

    feat: write app_name in psql

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -122,6 +122,7 @@ DATABASES = {
         "HOST": os.getenv("POSTGRES_HOST", "localhost"),
         "PORT": os.getenv("POSTGRES_PORT", 5432),
         "CONN_MAX_AGE": 60,
+        "OPTIONS": {"application_name": os.getenv("PGAPPNAME", "grms-web")},
     }
 }
 
