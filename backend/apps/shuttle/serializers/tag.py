8b6c53f123c10b7009c84628958e43810dfac609
bakha_aka <bt@room.io>
2025-10-12 22:12:13 +0200

    fix: added CLIENT_SCOPE in get attributes

--- backend/apps/shuttle/serializers/tag.py
+++ backend/apps/shuttle/serializers/tag.py
@@ -7,7 +7,14 @@ from shuttle.models import AttributeKv
 
 class TagFilterPath(ValidatorSerializer):
     device_id = serializers.PrimaryKeyRelatedField(queryset=Device.objects.all())
-    scope = serializers.ChoiceField(choices=[AttributeKv.SHARED_SCOPE, AttributeKv.SERVER_SCOPE, "LATEST_TELEMETRY"])
+    scope = serializers.ChoiceField(
+        choices=[
+            AttributeKv.SHARED_SCOPE,
+            AttributeKv.SERVER_SCOPE,
+            AttributeKv.CLIENT_SCOPE,
+            "LATEST_TELEMETRY",
+        ]
+    )
 
 
 class TagFilterParams(ValidatorSerializer):
