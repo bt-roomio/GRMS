033b9cb31226744f8c1cf811a9ecf8307309b79e
bakha_aka <bt@room.io>
2025-10-13 09:03:58 +0200

    lint: formatter

--- backend/apps/shuttle/querysets/attributes.py
+++ backend/apps/shuttle/querysets/attributes.py
@@ -1,5 +1,4 @@
-from django.db.models import F
-from django.db.models import Prefetch, Q
+from django.db.models import F, Prefetch, Q
 
 from core.querysets.base_queryset import BaseQuerySet
 
@@ -35,13 +34,11 @@ class AttributeKvQuerySet(BaseQuerySet):
             entity_id__in=devices
         ).update(long_v=None)
 
-
     def inactive_devices_in_spaces(self, tenant_id, sort_by="-last_update_ts"):
         from main.models import DevicePublicSpaces
         from shuttle.models import AttributeKv
 
         order = (sort_by,) if isinstance(sort_by, str) else tuple(sort_by or ("-last_update_ts",))
-
 
         qs = (
             self.filter(
@@ -60,9 +57,7 @@ class AttributeKvQuerySet(BaseQuerySet):
                 ),
                 Prefetch(
                     "entity__attribute_kvs",
-                    queryset=AttributeKv.objects.filter(
-                        attribute_key="ipAddress"
-                    ).order_by("-last_update_ts"),
+                    queryset=AttributeKv.objects.filter(attribute_key="ipAddress").order_by("-last_update_ts"),
                     to_attr="prefetched_ip_attrs",
                 ),
             )
