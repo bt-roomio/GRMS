4e32e6f1a1efade02c782134eb774c715a82d33d
bakha_aka <bt@room.io>
2025-10-27 10:39:12 +0200

    fix: create_relation command

--- backend/apps/core/management/commands/create_relation.py
+++ backend/apps/core/management/commands/create_relation.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from django.core.management.base import BaseCommand
 
 from main.models import Device
@@ -7,17 +9,33 @@ from shuttle.models import Relation
 class Command(BaseCommand):
     help = "Create Relation"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--confirm",
+            action="store_true",
+            help="Skip confirmation prompt (use with caution)",
+        )
+        parser.add_argument(
+            "tenant_id",
+            type=UUID,
+            help="Tenant",
+        )
+
     def handle(self, *args, **options):
         """Create Relation between gateway and other devices"""
-
-        tenant_id = "ea776673-3987-429f-b969-e9afb606dc8c"
+        tenant_id = options["tenant_id"]
+        confirm = options["confirm"]
 
         is_gateway = Device.objects.filter(tenant_id=tenant_id, is_active=True, additional_info__gateway=True).first()
         devices = Device.objects.filter(tenant_id=tenant_id, is_active=True).exclude(
             id=is_gateway.id  # pyright: ignore
         )
 
-        print(devices.count())
+        print(
+            f"Tenant: {tenant_id}, {devices.count()} device count will be connected to `{is_gateway.name}`"  # pyright: ignore
+        )
+        if not confirm:
+            return
 
         for device in devices:
             dd = {
@@ -29,3 +47,5 @@ class Command(BaseCommand):
                 "to_type": "DEVICE",
             }
             Relation.objects.get_or_create(**dd)
+
+        print("Successfully created relations")
