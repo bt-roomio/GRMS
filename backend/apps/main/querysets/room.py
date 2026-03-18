4b6fc805fb01d6378ed78628445edfd2bdc780bf
bakha_aka <bt@room.io>
2026-03-18 10:27:28 +0500

    feat(room): enhance search by device fields and labels
    - Extended RoomFilterParams to support searching by device name and
    label
      via new search fields ("device_name", "device_label").
    - Updated RoomQuerySet to allow searching both devices and door lock
    devices
      when device-related fields are used.
    - Added "label" as a valid search field for DeviceFilterParams.
    - Improved validation and mapping of search fields for room filtering.

--- backend/apps/main/querysets/room.py
+++ backend/apps/main/querysets/room.py
@@ -56,7 +56,14 @@ class RoomQuerySet(BaseQuerySet):
             query = query.filter(blocks_filter)
 
         if search_field and search_value:
-            query = query.filter(Q(**{f"{search_field}__istartswith": search_value}))
+            search_filter = Q(**{f"{search_field}__istartswith": search_value})
+
+            if search_field.startswith("devices__"):
+                door_lock_field = search_field.replace("devices__", "door_lock_device__")
+                search_filter = Q(**{f"{search_field}__istartswith": search_value, "devices__is_active": True}) | Q(
+                    **{f"{door_lock_field}__istartswith": search_value, "door_lock_device__is_active": True}
+                )
+            query = query.filter(search_filter)
 
         query = query.filter(status=status) if status else query
         return query.order_by(*(sort_by or ["block", "floor", "number"]) + ["id"])
