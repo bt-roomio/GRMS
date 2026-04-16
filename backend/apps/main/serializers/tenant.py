429ba6c5492ef706b0607c7fa91c4e8555335deb
bakha_aka <bt@room.io>
2026-04-16 14:40:31 +0500

    feat(api): added offline/total gateways in tenant API

--- backend/apps/main/serializers/tenant.py
+++ backend/apps/main/serializers/tenant.py
@@ -41,6 +41,8 @@ class TenantSerializer(serializers.ModelSerializer):
         data["online_rooms"] = instance.online_rooms if hasattr(instance, "online_rooms") else 0
         data["offline_rooms"] = instance.offline_rooms if hasattr(instance, "offline_rooms") else 0
         data["total_rooms"] = instance.total_rooms if hasattr(instance, "total_rooms") else 0
+        data["offline_gateways"] = instance.offline_gateways if hasattr(instance, "offline_gateways") else 0
+        data["total_gateways"] = instance.total_gateways if hasattr(instance, "total_gateways") else 0
         return data
 
     class Meta:
@@ -48,17 +50,17 @@ class TenantSerializer(serializers.ModelSerializer):
         fields = (
             "id",
             "created_at",
+            "title",
+            "email",
             "tenant_profile",
             "additional_info",
             "address",
             "address2",
             "city",
             "country",
-            "email",
             "phone",
             "region",
             "state",
-            "title",
             "zip",
         )
 
